18784beeddd98615aa79cc787a0174f7ba529095
bala-amypo <bala@amypo.in>
2025-12-18 04:31:11 +0000

    User commit

--- src/main/java/com/example/demo/model/UserProfile.java
+++ src/main/java/com/example/demo/model/UserProfile.java
@@ -22,5 +22,13 @@ public class UserProfile{
     public String getUsername(){
         return username;
     }
-    public 
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
 }

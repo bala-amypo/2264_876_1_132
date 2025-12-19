f4261627b4f5a10e87714e40d291ca201d006cb8
bala-amypo <bala@amypo.in>
2025-12-19 09:12:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillCategoryRepository.java
+++ src/main/java/com/example/demo/repository/SkillCategoryRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.barter.model.SkillCategory;
+import com.example.demo.model.SkillCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;

cfab8ab54a9c234335b185509c4b414746bf50a7
bala-amypo <bala@amypo.in>
2025-12-24 04:07:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/UserProfileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class UserProfileNotFoundException extends RuntimeException {
+
+    public UserProfileNotFoundException() {
+        super("UserProfile not found");
+    }
+}
